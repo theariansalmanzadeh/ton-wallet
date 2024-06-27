e46567511f7ffde47f4203e389d8c814b0b4e35b
ArianEVM <109427302+theariansalmanzadeh@users.noreply.github.com>
2024-06-27 17:51:58 +0330

    telegram wrapper uncommented

--- ton-wallet/src/App.tsx
+++ ton-wallet/src/App.tsx
@@ -1,22 +1,22 @@
 import Slider from "./components/Slider";
 // import BallAnimation from "./components/BallAnimation";
 import WalletDetails from "./components/WalletDetails";
-// import TelegramWrapper from "./components/TelegramWrapper";
+import TelegramWrapper from "./components/TelegramWrapper";
 import "./App.css";
 
 function App() {
   return (
     <>
-      {/* <TelegramWrapper> */}
-      <div>
-        <p>A quick demo with animations and ton sdk in telegram</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-      <Slider />
-      <WalletDetails />
-      {/* </TelegramWrapper> */}
+      <TelegramWrapper>
+        <div>
+          <p>A quick demo with animations and ton sdk in telegram</p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and Preact logos to learn more
+        </p>
+        <Slider />
+        <WalletDetails />
+      </TelegramWrapper>
     </>
   );
 }
